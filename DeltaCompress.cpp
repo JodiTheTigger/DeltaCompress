9c1d6a068896df7d0083fb87c1de8d8e8d3eb757
Richard Maxwell <jodi.the.tigger@gmail.com>
2015-03-20 16:47:10 +1300

    Setup constraints, get stats from file.

--- DeltaCompress.cpp
+++ DeltaCompress.cpp
@@ -36,11 +36,6 @@ ScopeExit<F> MakeScopeExit(F f) {
 
 // //////////////////////////////////////////////////////
 
-// Still making up my mind on the best way to assert.
-#define ASSERT_TRUE(x) assert(x)
-
-// //////////////////////////////////////////////////////
-
 struct DeltaData
 {
     int orientation_largest;
@@ -53,10 +48,127 @@ struct DeltaData
     int interacting;
 };
 
+inline bool operator==(const DeltaData& lhs, const DeltaData& rhs)
+{
+    return
+    (
+            (lhs.interacting    == rhs.interacting)
+        &&  (lhs.orientation_a  == rhs.orientation_a)
+        &&  (lhs.orientation_b  == rhs.orientation_b)
+        &&  (lhs.orientation_c  == rhs.orientation_c)
+        &&  (lhs.position_x     == rhs.position_x)
+        &&  (lhs.position_y     == rhs.position_y)
+        &&  (lhs.position_z     == rhs.position_z)
+    );
+}
+
+inline bool operator!=(const DeltaData& lhs, const DeltaData& rhs){return !operator==(lhs,rhs);}
+
 static const size_t Cubes = 901;
+
+// Using contraints from
+// http://gafferongames.com/networked-physics/snapshot-compression/
+
+// With this technique I’ve found that minimum sufficient precision for
+// my simulation is 9 bits per-smallest component. This gives a result
+// of 2 + 9 + 9 + 9 = 29 bits per-orientation (originally 128!).
+
+static const unsigned RotationMaxBits = 9;
+static const unsigned RotationIndexMaxBits = 2;
+
+// I found a maximum speed of 32 meters per-second is a nice power of two
+// and doesn’t negatively affect the player experience in the cube simulation.
+static const unsigned MaxSpeedMetersPerSecond = 32;
+
+// Now we have only position to compress. We’ll use the same trick that we used
+// for linear velocity: bound and quantize. Most game worlds are reasonably big
+// so I chose a position bound of [-256,255] meters in the horizontal plane (xy)
+// and since in my cube simulation the floor is at z=0, I chose for z a range of
+// [0,32] meters.
+
+// Now we need to work out how much precision is required. With some
+// experimentation I found that 512 values per-meter (roughly 0.5mm precision)
+// provides sufficient precision. This gives position x and y components
+// in [-131072,+131071] and z components in range [0,16383].
+
+// RAM: NOTE: ambiguous statement. So I'll use [-131072,+131071]
+
+static const unsigned   ValuesPerMeter    = 512;
+static const unsigned   MaxZInclusize     = (32 * ValuesPerMeter) - 1;
+static const unsigned   MinZInclusize     = 0;
+static const int        MaxXYInclusize    = 131071;  // (256 * 512) - 1
+static const int        MinXYInclusize    = -131072; // (-256 * 512)
+static const int        XYRange           = MaxXYInclusize - MinXYInclusize;
+
+static const unsigned   MaxBitsZ          = 14;
+static const unsigned   MaxBitsXY         = 18;
+
+static const unsigned   MaxSnapshotsPerSecond = 60;
+
+static const unsigned   FirstBase           = 6;
+static const unsigned   PacketDelta         = 6;
+
+// This one is important
+static const float      MaxPositionChangePerSnapshot =
+        MaxSpeedMetersPerSecond * ValuesPerMeter /
+        (float) MaxSnapshotsPerSecond;
+
+static_assert(
+    ((1 << MaxBitsZ) - 1) == MaxZInclusize,
+    "MaxBitZ doesn't match MaxZInclusize");
+
+static_assert(
+    ((1 << MaxBitsXY) - 1) == XYRange,
+    "MaxBitsXY doesn't match XYRange");
 
 typedef std::array<DeltaData, Cubes> Frame;
 
+// //////////////////////////////////////////////////////
+struct Stats
+{
+    unsigned itemCount;
+
+    unsigned notChangedCount;
+
+    unsigned interactingTotal;
+    unsigned interactingNotChanged;
+    unsigned notInteractingNotChanged;
+};
+
+void DoSomeStats(const Frame& base, const Frame& target, Stats& stats)
+{
+    assert(base.size() == target.size());
+    assert(!base.empty());
+
+    auto size = base.size();
+    for (size_t i = 0; i < size; ++i)
+    {
+        stats.itemCount++;
+
+        if (base[i] == target[i])
+        {
+            stats.notChangedCount++;
+        }
+
+        if (base[i].interacting)
+        {
+            stats.interactingTotal++;
+
+            if (base[i].interacting == target[i].interacting)
+            {
+                stats.interactingNotChanged++;
+            }
+        }
+
+        if (!base[i].interacting)
+        {
+            if (base[i].interacting == target[i].interacting)
+            {
+                stats.notInteractingNotChanged++;
+            }
+        }
+    }
+}
 
 // //////////////////////////////////////////////////////
 
@@ -67,6 +179,7 @@ int main(int, char**)
         const auto fileHandle = fopen("delta_data.bin", "rb");
         if (!fileHandle)
         {
+            printf("ERROR: Cannot open 'delta_data.bin' for reading.\n");
             return {};
         }
         SCOPED_EXIT(fclose(fileHandle));
@@ -91,9 +204,34 @@ int main(int, char**)
 
     if (frames.empty())
     {
-        printf("ERROR: Cannot open 'delta_data.bin' for reading.\n");
         return 1;
     }
+
+    // Lets do some research
+    auto size = frames.size();
+    Stats stats = {0};
+    for (size_t i = FirstBase; i < size; ++i)
+    {
+         DoSomeStats(frames[i-FirstBase], frames[i], stats);
+    }
+
+    #define PRINT_INT(x) printf("%-32s\t%d\n", #x, x);
+
+    PRINT_INT(stats.itemCount)
+    PRINT_INT(stats.notChangedCount)
+    PRINT_INT(stats.interactingTotal)
+    PRINT_INT(stats.interactingNotChanged)
+    PRINT_INT(stats.notInteractingNotChanged)
+
+    auto percentUnchanged = (100.0f * stats.notChangedCount) / stats.itemCount;
+    auto percentIUnchanged = (100.0f * stats.interactingNotChanged) / stats.interactingTotal;
+    auto percentSUnchanged = (100.0f * stats.notInteractingNotChanged) / (stats.itemCount - stats.interactingTotal);
+
+    #define PRINT_FLOAT(x) printf("%-32s\t%f\n", #x, x);
+
+    PRINT_FLOAT(percentUnchanged)
+    PRINT_FLOAT(percentIUnchanged)
+    PRINT_FLOAT(percentSUnchanged)
 
     return 0;
 }
