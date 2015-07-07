68dc8fe51312993981250c25007f07ac368e0cdd
Richard Maxwell <jodi.the.tigger@gmail.com>
2015-07-07 20:35:47 +1200

    Got rid of bit mask lookup as it wasn't used anywhere.

--- DeltaCompress.cpp
+++ DeltaCompress.cpp
@@ -903,56 +903,6 @@ unsigned MinBits(unsigned value)
     return result;
 }
 
-
-static const constexpr unsigned bitmask_lookup[] =
-{
-    0x00000000,
-    0x00000001,
-    0x00000003,
-    0x00000007,
-    0x0000000F,
-
-    0x0000001F,
-    0x0000003F,
-    0x0000007F,
-    0x000000FF,
-
-    0x000001FF,
-    0x000003FF,
-    0x000007FF,
-    0x00000FFF,
-
-    0x00001FFF,
-    0x00003FFF,
-    0x00007FFF,
-    0x0000FFFF,
-
-    0x0001FFFF,
-    0x0003FFFF,
-    0x0007FFFF,
-    0x000FFFFF,
-
-    0x001FFFFF,
-    0x003FFFFF,
-    0x007FFFFF,
-    0x00FFFFFF,
-
-    0x01FFFFFF,
-    0x03FFFFFF,
-    0x07FFFFFF,
-    0x0FFFFFFF,
-
-    0x1FFFFFFF,
-    0x3FFFFFFF,
-    0x7FFFFFFF,
-    0xFFFFFFFF,
-};
-
-inline constexpr auto bit_mask(unsigned bits) -> unsigned
-{
-    return bitmask_lookup[bits];
-}
-
 // //////////////////////////////////////////////////////
 
 using namespace Range_models;
